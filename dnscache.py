49299a818648bea0d4bb53f6f21d0ad6e01cd5e0
Geir Skjotskift <geir@mnemonic.no>
2017-02-02 10:44:23 +0100

    Initial checkin.

---
+++ dnscache.py
@@ -0,0 +1,206 @@
+# Copyright (c) 2017, mnemonic AS <geir@mnemonic.no>
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE
+
+"""This module attempts to extract the DNS cache from a Windows image
+
+@author:        Geir Skjotskift
+@license:       ISC License
+@contact:       geir@mnemonic.no
+
+DEPENDENCIES:
+    construct (pdbparse dependency)
+    pdbparse
+
+REFERENCES:
+    [1] Cohen, M. (2014). The Windows User mode heap and the DNS resolver cache.
+        Retrieved from:
+          http://www.rekall-forensic.com/posts/2014-12-20-usermode-heap.html
+    [2] Cohen, M. (2014). Source code for Module rekall.plugins.windows.dns
+        Retrieved from:
+          http://www.rekall-forensic.com/epydocs/rekall.plugins.windows.dns-pysrc.html
+    [3] Pulley, C. (2013). Source code for Module symbols.py (volatility community plugins)
+        Retrieved from:
+          https://github.com/carlpulley/volatility/blob/master/symbols.py
+    [4] Ligh, M., Case, A., Levy, J. & Walters, A. (2014). The Art of Memory Forensics.
+    [5] Levy, J. (2015). dns cache plugin #201 (Volatility Issiues)
+        Retrieved from:
+          https://github.com/volatilityfoundation/volatility/issues/201
+"""
+
+import volatility.debug as debug
+import volatility.plugins.common as common
+import volatility.utils as utils
+import volatility.win32 as win32
+import pdbparse
+import pdbparse.peinfo
+
+
+class DNSCache(common.AbstractWindowsCommand):
+    """Volatility plugin to extract the Windows DNS cache"""
+
+    def __init__(self, config, *args, **kwargs):
+
+        common.AbstractWindowsCommand.__init__(self, config, *args, **kwargs)
+
+    def _find_dns_resolver(self, ps_list):
+
+        for proc in ps_list:
+            for mod in proc.get_load_modules():
+                if mod.m("BaseDllName") == "dnsrslvr.dll":
+                    yield proc, mod
+
+    def _is_valid_debug_dir(self, debug_dir, image_base, addr_space):
+        if debug_dir == None:
+            return False
+
+        if debug_dir.AddressOfRawData == 0:
+            return False
+
+        if not addr_space.is_valid_address(image_base + debug_dir.AddressOfRawData):
+            return False
+
+        if not addr_space.is_valid_address(image_base + debug_dir.AddressOfRawData + debug_dir.SizeOfData - 1):
+            return False
+
+        return True
+
+    def _get_debug_symbols(self, addr_space, mod):
+
+        image_base = mod.DllBase
+        debug_dir = mod.get_debug_directory()
+
+        if not self._is_valid_debug_dir(debug_dir, image_base, addr_space):
+            debug.info("Invalid debug dir {0} {1} {2}".format(hex(debug_dir.v()), hex(image_base.v()), hex(addr_space.v())))
+            return
+
+        debug.info("Found debug_dir: {0}, image_base: {1}".format(hex(debug_dir.v()), hex(image_base.v())))
+        debug_data = addr_space.zread(image_base + debug_dir.AddressOfRawData, debug_dir.SizeOfData)
+        if debug_data[:4] == 'RSDS':
+            return pdbparse.peinfo.get_rsds(debug_data)
+
+        debug.info("Found no RSDS")
+
+    def calculate(self):
+
+        address_space = utils.load_as(self._config)
+        ps_list = win32.tasks.pslist(address_space)
+
+        for proc, mod in self._find_dns_resolver(ps_list):
+
+            debug.info("Found PID: {0} Dll: {1}".format(proc.UniqueProcessId, str(mod.m("FullDllName"))))
+            guid, pdb = self._get_debug_symbols(proc.get_process_address_space(), mod)
+            yield guid, pdb
+
+    def render_text(self, outfd, data):
+        outfd.write("DEBUG!***\n")
+        for guid, pdb in data:
+            outfd.write("DEBUG -- GUID: {0}, PDB: {1}\n".format(guid, pdb))
+
+
+class IPv4DWORD(object):
+    """IPv4DWORD is a wrapper object for a DWORD (uint32) representing)
+    an IP version 4 address. The original value is stored in the value
+    attribute, but it will be represented as a string in human readable
+    form"""
+
+    def __init__(self, value):
+        """Constructor taking a DWORD as argument"""
+
+        self.value = value
+
+    def _IPv4FromDWORD(self):
+        """_IPv4FromDWORD takes converst the DWORD representation
+        into a human readble IPv4 form"""
+
+        a = self.value & 0xff
+        b = (self.value & 0xff00) >> 8
+        c = (self.value & 0xff0000) >> 16
+        d = (self.value & 0xff000000) >> 24
+        return "%d.%d.%d.%d" % (a,b,c,d)
+
+    def __str__(self):
+        return self._IPv4FromDWORD()
+
+
+# DNS type Enumeration
+DNSPType = {
+    "QUESTION": 0,
+    "ANSWER": 1,
+    "AUTHORITY": 2,
+    "ADDITIONAL": 3
+}
+
+# DNS Record Type Enumeration
+DNSType = {
+    "A": 0x0001,
+    "NS_TYPE_NS": 0x0002,
+    "MD": 0x0003,
+    "MF": 0x0004,
+    "CNAME": 0x0005,
+    "SOA": 0x0006,
+    "MB": 0x0007,
+    "MG": 0x0008,
+    "MR": 0x0009,
+    "NULL": 0x000a,
+    "WKS": 0x000b,
+    "PTR": 0x000c,
+    "HINFO": 0x000d,
+    "MINFO": 0x000e,
+    "MX": 0x000f,
+    "TEXT": 0x0010,
+    "RP": 0x0011,
+    "AFSDB": 0x0012,
+    "X25": 0x0013,
+    "ISDN": 0x0014,
+    "RT": 0x0015,
+    "NSAP": 0x0016,
+    "NSAPPTR": 0x0017,
+    "SIG": 0x0018,
+    "KEY": 0x0019,
+    "PX": 0x001a,
+    "GPOS": 0x001b,
+    "AAAA": 0x001c,
+    "LOC": 0x001d,
+    "NXT": 0x001e,
+    "EID": 0x001f,
+    "NIMLOC": 0x0020,
+    "SRV": 0x0021,
+    "ATMA": 0x0022,
+    "NAPTR": 0x0023,
+    "KX": 0x0024,
+    "CERT": 0x0025,
+    "A6": 0x0026,
+    "DNAME": 0x0027,
+    "SINK": 0x0028,
+    "OPT": 0x0029,
+    "UINFO": 0x0064,
+    "UID": 0x0065,
+    "GID": 0x0066,
+    "UNSPEC": 0x0067,
+    "ADDRS": 0x00f8,
+    "TKEY": 0x00f9,
+    "TSIG": 0x00fa,
+    "IXFR": 0x00fb,
+    "AXFR": 0x00fc,
+    "MAILB": 0x00fd,
+    "MAILA": 0x00fe,
+    "ALL": 0x00ff,
+    "ANY": 0x00ff
+}
